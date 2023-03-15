01ae95374e9a5d40e65e2e1bcea8f5eedbcf4096
Félix Carrasco Andrés <felixcarrasco39@gmail.com>
2023-03-15 12:50:24 +0100

    PRUEB-1 Cambio de nombres

--- demo/src/main/java/com/eoi/springboot/MySpringBootApplication.java
+++ demo/src/main/java/com/eoi/springboot/MySpringBootApplication.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.eoi.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,11 +7,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-public class DemoApplication {
+public class MySpringBootApplication {
 
 	public static void main(String[] args) {
 
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(MySpringBootApplication.class, args);
 	}
 
 
